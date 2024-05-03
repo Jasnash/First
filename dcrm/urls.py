cd253124926e913d3ecc6acb60f220f078b211d9
Balabbo <balabbonv@students.national-u.edu.ph>
2024-05-03 17:31:19 +0800

    registration

--- dcrm/urls.py
+++ dcrm/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [ 
  path('mypage', views.home, name='home'),   
+ path('register', views.register_user, name='register')
 ] 
--- dcrm/views.py
+++ dcrm/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from.forms import SignUpForm
+from django.contrib import messages
 # Create your views here.
 
 
 def home(request):
     #do something
     return render(request, 'home.html')
+
+def register_user(request):
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        #authenticate
+        username = request.POST('username')
+        password = request.POST('password')
+        user = authenticate(request, username=username, password=password)
+
+       
+        login(request, user)
+        return redirect('home')
+    else:
+        form =SignUpForm()
+        return render(request, 'register.html',{'form': form})
+
+    return render(request, 'register.html',{'form': form})
